b5ac936b4dafab2b8777ef62603cbaa57349f3d4
Brenna Pacheco <brennapacheco@hotmail.com>
2022-09-29 14:43:27 -0300

    Project setup

---
+++ WebBrowser/WebBrowser/Screens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebBrowser
+//
+//  Created by Brenna Pacheco da Silva Alves on 29/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
